75b3fc0a3729dde9cb6ffe0616499fecfdba5767
AnastasiaKuznetsova2806 <akukuznetsova@yandex.ru>
2023-01-31 16:43:03 +0300

    feat: Добавлены тесты для пакета request

---
+++ mainservice/src/test/java/ewmmainservice/request/dto/ParticipationRequestDtoTest.java
@@ -0,0 +1,37 @@
+package ewmmainservice.request.dto;
+
+import ewmmainservice.request.Status;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.JsonTest;
+import org.springframework.boot.test.json.JacksonTester;
+import org.springframework.boot.test.json.JsonContent;
+
+import java.time.LocalDateTime;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+@JsonTest
+class ParticipationRequestDtoTest {
+    @Autowired
+    JacksonTester<ParticipationRequestDto> json;
+
+    @Test
+    void participationRequestDto() throws Exception {
+        LocalDateTime dateTime = LocalDateTime.now().withNano(0);
+        ParticipationRequestDto requestDto = new ParticipationRequestDto(
+                dateTime, 1L, 1L, 1L, Status.PENDING
+        );
+
+        JsonContent<ParticipationRequestDto> result = json.write(requestDto);
+
+        assertThat(result).extractingJsonPathValue("$.created").isEqualTo(dateTime.toString());
+        assertThat(result).extractingJsonPathNumberValue("$.event").isEqualTo(1);
+        assertThat(result).extractingJsonPathNumberValue("$.id").isEqualTo(1);
+        assertThat(result).extractingJsonPathNumberValue("$.requester").isEqualTo(1);
+        assertThat(result).extractingJsonPathValue("$.status").isEqualTo(Status.PENDING.toString());
+    }
+
+}
